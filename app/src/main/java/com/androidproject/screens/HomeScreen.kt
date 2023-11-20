5de33e4a0da73f62be0f5516f13a0d1742024a96
FedDanPav <fedor.danilov@student.hogent.be>
2023-11-20 20:09:28 +0100

    Edit styling of text on HomeScreen

--- app/src/main/java/com/androidproject/screens/HomeScreen.kt
+++ app/src/main/java/com/androidproject/screens/HomeScreen.kt
@@ -8,10 +8,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Label
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,15 +30,27 @@ fun HomeScreen(paddingValues: PaddingValues) {
     ) {
         Label(
             label = { }) {
-                Text(text = "Welcome to the Home Page")
+                Text(
+                    text = "Welcome to the Home Page",
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold
+                )
         }
         Label(
             label = { }) {
-            Text(text = "Here you'll be able to find random movies to watch")
+            Text(
+                text = "Here you'll be able to find random movies to watch",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold
+            )
         }
         Label(
             label = { }) {
-            Text(text = "Just use the bottom menu to start searching for your movie!")
+            Text(
+                text = "Use the bottom menu to start searching!",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
