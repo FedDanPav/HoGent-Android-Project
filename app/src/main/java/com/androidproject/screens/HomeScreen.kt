5032383d8b8c37077560d7ced41ea66e3ae242b4
FedDanPav <fedor.danilov@student.hogent.be>
2023-11-20 12:57:22 +0100

    Create basic text for HomeScreen

--- app/src/main/java/com/androidproject/screens/HomeScreen.kt
+++ app/src/main/java/com/androidproject/screens/HomeScreen.kt
@@ -1,19 +1,42 @@
 package com.androidproject.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Label
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(paddingValues: PaddingValues) {
-    Box(
-        modifier = Modifier.padding(paddingValues)
+    Column(
+        modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text("Home Page")
+        Label(
+            label = { }) {
+                Text(text = "Welcome to the Home Page")
+        }
+        Label(
+            label = { }) {
+            Text(text = "Here you'll be able to find random movies to watch")
+        }
+        Label(
+            label = { }) {
+            Text(text = "Just use the bottom menu to start searching for your movie!")
+        }
     }
 }
 
