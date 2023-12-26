c21ff73865ed0a2086ed28253ad9e803107bc329
FedDanPav <fedor.danilov@student.hogent.be>
2023-12-26 14:08:16 +0100

    Fix GenreDto not having a toDomainObject emthod

--- app/src/main/java/com/androidproject/data/remote/dto/GenreDto.kt
+++ app/src/main/java/com/androidproject/data/remote/dto/GenreDto.kt
@@ -1,6 +1,15 @@
 package com.androidproject.data.remote.dto
 
+import com.androidproject.model.Genre
+
 data class GenreDto(
     val id : Int,
     val name : String
 )
+
+fun GenreDto.toDomainObject(): Genre {
+    return Genre(
+        id = this.id,
+        name = this.name
+    )
+}
