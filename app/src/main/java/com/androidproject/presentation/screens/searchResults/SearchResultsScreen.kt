3c1c165d5ae2dad0a47021da1da8132a42745fbe
FedDanPav <fedor.danilov@student.hogent.be>
2023-12-27 19:26:02 +0100

    Hook up both states to the SearchResultsScreen

--- app/src/main/java/com/androidproject/presentation/screens/searchResults/SearchResultsScreen.kt
+++ app/src/main/java/com/androidproject/presentation/screens/searchResults/SearchResultsScreen.kt
@@ -1,12 +1,20 @@
 package com.androidproject.presentation.screens.searchResults
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.androidproject.R
 import com.androidproject.model.Genre
 import com.androidproject.model.Movie
+import com.androidproject.presentation.screens.misc.ErrorScreen
+import com.androidproject.presentation.screens.misc.LoadingScreen
 import com.androidproject.util.Resource
 
 @Composable
@@ -26,10 +34,58 @@ fun SearchResultsScreen(
     )
 }
 
+@Composable
 fun SearchResultsScreen(
     paddingValues : PaddingValues,
     genreUiState : Resource<List<Genre>>,
     movieUiState : Resource<List<Movie>>
+) {
+    Column (
+
+        modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize()
+    ) {
+        when (genreUiState) {
+            is Resource.Loading -> {
+                LoadingScreen()
+            }
+
+            is Resource.Error -> {
+                ErrorScreen(
+                    error = genreUiState.message ?: stringResource(R.string.unknownerror)
+                )
+            }
+
+            is Resource.Success -> {
+                genreUiState.data?.let { genreList ->
+                    when (movieUiState) {
+                        is Resource.Loading -> {
+                            LoadingScreen()
+                        }
+
+                        is Resource.Error -> {
+                            ErrorScreen(
+                                error = movieUiState.message ?: stringResource(R.string.unknownerror)
+                            )
+                        }
+
+                        is Resource.Success -> {
+                            movieUiState.data?.let {
+                                ResultOverview(genres = genreList, movies = it)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ResultOverview(
+    genres : List<Genre>,
+    movies : List<Movie>
 ) {
 
 }
