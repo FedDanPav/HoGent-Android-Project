87dbc9ee86f644871942009308b2d59f960b59bf
FedDanPav <fedor.danilov@student.hogent.be>
2023-12-29 13:15:28 +0100

    Add Room + MovieEntity

---
+++ app/src/main/java/com/androidproject/data/local/entity/MovieEntity.kt
@@ -0,0 +1,27 @@
+package com.androidproject.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.androidproject.model.Movie
+
+@Entity(tableName = "saved_movie")
+data class MovieEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val title : String,
+    val overview : String,
+    val originalLanguage : String,
+    val voteAverage : Float,
+    val voteCount : Int,
+    val genreIds : List<Int>
+)
+
+fun MovieEntity.toDisease(): Movie {
+    return Movie(
+        title = this.title,
+        overview = this.overview,
+        originalLanguage = this.originalLanguage,
+        voteAverage = this.voteAverage,
+        voteCount = this.voteCount,
+        genreIds = this.genreIds
+    )
+}
