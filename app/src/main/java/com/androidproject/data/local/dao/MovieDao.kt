7e167f576e49b6e750bd977bf339d297f09f57fd
FedDanPav <fedor.danilov@student.hogent.be>
2023-12-29 22:44:10 +0100

    Add delete actions to Dao

--- app/src/main/java/com/androidproject/data/local/dao/MovieDao.kt
+++ app/src/main/java/com/androidproject/data/local/dao/MovieDao.kt
@@ -1,6 +1,7 @@
 package com.androidproject.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import com.androidproject.data.local.entity.MovieEntity
@@ -8,8 +9,14 @@ import com.androidproject.data.local.entity.MovieEntity
 @Dao
 interface MovieDao {
     @Upsert
-    suspend fun upsertMovies(movie: MovieEntity)
+    suspend fun upsertMovie(movie: MovieEntity)
+
+    @Delete
+    suspend fun deleteMovie(movie: MovieEntity)
 
     @Query("SELECT * FROM movies")
     suspend fun getMovies(): List<MovieEntity>
+
+    @Query("SELECT * FROM movies WHERE movieId = :movieId")
+    suspend fun getMovieById(movieId : Int) : List<MovieEntity>
 }
--- app/src/main/java/com/androidproject/data/local/dao/MovieToGenreDao.kt
+++ app/src/main/java/com/androidproject/data/local/dao/MovieToGenreDao.kt
@@ -1,6 +1,7 @@
 package com.androidproject.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import com.androidproject.data.local.entity.MovieToGenreEntity
@@ -9,6 +10,9 @@ import com.androidproject.data.local.entity.MovieToGenreEntity
 interface MovieToGenreDao {
     @Upsert
     suspend fun upsertMoviesToGenres(mte : List<MovieToGenreEntity>)
+
+    @Delete
+    suspend fun deleteMoviesToGenres(mte : List<MovieToGenreEntity>)
 
     @Query("SELECT * FROM movies_to_genres WHERE tbl_movieId = :movieId")
     suspend fun getGenresByMovieId(movieId : Int) : List<MovieToGenreEntity>
